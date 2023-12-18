dddc66eb00d11d8056124fe357a10c59652f39fd
LeMinhTien0912 <154213492+LeMinhTien0912@users.noreply.github.com>
2023-12-18 15:55:27 +0700

    Update sendemail.php

--- sendemail.php
+++ sendemail.php
@@ -10,8 +10,8 @@
 
    class SendEMail{
        public function send($username,$email,$title,$content){
-        $account = "truongsendmail@gmail.com";
-        $password="truongjae27";
+        $account = "adminsendmail@gmail.com";
+        $password="admin123";
         $mail = new PHPMailer(true);
         try{
             $mail->SMTPDebug = 0;
