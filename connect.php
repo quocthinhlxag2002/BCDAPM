18a2f7a82d8437a8883e8396e07009aa078e43c5
quocthinhlxag2002 <153052907+quocthinhlxag2002@users.noreply.github.com>
2023-12-18 15:19:43 +0700

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,8 +1,57 @@
 <?php
-$server = "localhost:3309";
-$user="root";
-$pass="";
-$database="shoe";
-$conn=mysqli_connect($server,$user,$pass,$database);
-mysqli_query($conn,'set names "utf8"');
-?>
+class DATABASE{
+    private static $dns = "mysql:host=localhost;dbname=shoe;port=3306";
+    private static $username = "root";
+    private static $password = "";
+    private static $options = array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION, 
+                                    PDO::MYSQL_ATTR_INIT_COMMAND=>"SET NAMES utf8");    
+    private static $db;
+    
+    private function __construct(){} 
+    
+    public static function connect(){
+        if(!isset(self::$db)){
+            try{
+                self::$db = new PDO(self::$dns, 
+                                    self::$username, 
+                                    self::$password, 
+                                    self::$options);
+            }
+            catch(PDOException $e){
+                $error_message = $e->getMessage();
+                echo "<p>Lỗi kết nối: $error_message</p>";
+                exit();
+            }
+        }
+        return self::$db;
+    }
+    
+    public static function close(){
+        self::$db = null;
+    }
+
+    public static function execute_nonquery($sql, $option = array()) {
+        self::getDB();
+        if (self::$db != null) {
+            try {
+                $cmd = self::$db->prepare($sql);
+                if (count($option) > 0) {
+                    for ($i = 0; $i < count($option); $i++) {
+                        $cmd->bindParam($i + 1, $option[$i]);
+                    }
+                }
+                $cmd->execute();
+                $ketqua = $cmd->fetchAll();
+                return $ketqua;
+            } catch (PDOException $ex) {
+                ShowError($ex);
+            }
+        } else {
+            ShowError("Lỗi kết nối cơ sở dữ liệu");
+        }
+        self::disconnect();
+    }
+
+
+
+}
